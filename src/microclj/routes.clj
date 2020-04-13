1cce135735f170c97defec6cf15a9772119e5a31
Ryan Marples <ryan.marples@allocadia.com>
2020-04-13 12:54:33 -0700

    Add serving of static files

--- src/microclj/routes.clj
+++ src/microclj/routes.clj
@@ -1,11 +1,13 @@
 (ns microclj.routes
   (:require [compojure.core :refer :all]
-            [compojure.handler :as handler]))
+            [compojure.handler :as handler]
+            [compojure.route :as route]))
 
 (defn index-page [request]
   (str "Hello to " (get-in request [:context :trace-id])))
 
 (defroutes site-routes
-  (GET "/" request (index-page request)))
+  (GET "/" request (index-page request))
+  (route/resources "/"))
 
 (def site (handler/site site-routes))
