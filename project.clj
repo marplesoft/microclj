552803fcc528f2c466d4472b6de35d8abacbf55f
Ryan Marples <ryan.marples@allocadia.com>
2020-04-20 15:44:17 -0700

    Further logging and init tweaks

--- project.clj
+++ project.clj
@@ -4,16 +4,17 @@
                  [ring/ring-jetty-adapter "1.8.0"]
                  [compojure "1.6.1"]
                  [org.clojure/tools.logging "1.0.0"]
-                 [org.slf4j/slf4j-log4j12 "1.7.30"]
                  [log4j "1.2.15" :exclusions [javax.mail/mail
                                               javax.jms/jms
                                               com.sun.jdmk/jmxtools
                                               com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.7.30"]
                  [environ "1.1.0"]]
   :dev-dependencies [[ring/ring-devel "1.8.0"]]
   :plugins [[lein-ring "0.12.5"]
             [lein-environ "1.1.0"]]
-  :ring {:handler microclj.core/app}
+  :ring {:handler microclj.core/app
+         :init microclj.core/init}
   :profiles {:dev {:env {:mode "dev"}}}
   :main microclj.core
   :aot [microclj.core])
--- src/microclj/core.clj
+++ src/microclj/core.clj
@@ -2,19 +2,20 @@
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [microclj.middleware :refer [wrap-middlewares]]
             [microclj.routes :refer [site]]
-            [microclj.env :refer [check-for-missing-env]]
-            [clojure.tools.logging :refer [error]]
+            [microclj.env :refer [check-for-missing-env get-env]]
+            [clojure.tools.logging :refer [error info]]
             [clojure.string :refer [join]])
   (:gen-class :main true))
 
-(def app (wrap-middlewares site))
-
-(defn -main [& _]
+(defn init []
   (let [missing-vars (check-for-missing-env)]
     (when (not-empty missing-vars)
       (error (str "Required environment vars missing: " (join "," missing-vars)))
       (System/exit -1)))
-  (run-jetty app {:port 3000}))
+  (info (format "Starting microclj in '%s' mode" (get-env :mode))))
 
-(comment
-  (-main))
+(def app (wrap-middlewares site))
+
+(defn -main [& _]
+  (init)
+  (run-jetty app {:port 3000}))
