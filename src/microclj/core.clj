6727b02627ec4256bd6ccbe26d3196664b7c0449
Ryan Marples <ryan.marples@allocadia.com>
2020-04-21 10:13:09 -0700

    Just expose raw routes, not a compojure "site"

--- src/microclj/core.clj
+++ src/microclj/core.clj
@@ -1,7 +1,7 @@
 (ns microclj.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [microclj.middleware :refer [wrap-middlewares]]
-            [microclj.routes :refer [site]]
+            [microclj.routes :refer [site site-routes]]
             [microclj.env :as env]
             [clojure.tools.logging :refer [error info]])
   (:gen-class :main true))
@@ -9,7 +9,7 @@
 (defn init []
   (env/init))
 
-(def app (wrap-middlewares site))
+(def app (wrap-middlewares site-routes))
 
 (defn -main [& _]
   (init)
