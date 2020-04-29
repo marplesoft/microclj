fe4d8c517ef3a40d3b80d80f518702e9de86e32a
Ryan Marples <ryan.marples@allocadia.com>
2020-04-29 11:13:10 -0700

    Added global 404 check

--- src/microclj/config.clj
+++ src/microclj/config.clj
@@ -1,7 +1,9 @@
 (ns microclj.config
   (:require [microclj.first.app :as first]
-            [compojure.core :refer [routes context]]))
+            [compojure.core :refer [routes context]]
+            [compojure.route :as route]))
 
 (def all-routes
   (routes
-   (context "/first" [] first/app-routes))) 
+   (context "/first" [] first/app-routes)
+   (route/not-found "<h1>Page not found</h1>"))) 
